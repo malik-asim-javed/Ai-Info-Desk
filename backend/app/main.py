444c8aab3082e5c24c18457bcb73e195550790d5
Arileh <asimbscs235@gmail.com>
2026-07-15 00:35:52 +0500

    Use environment variable for API URL

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://ai-info-desk.vercel.app/",
+        "https://ai-info-desk.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
